0c22fbc22252df8f726ea781d4008d4b456468ae
Rishik <rishikrc3@gmail.com>
2024-03-15 10:52:47 +0530

    bc

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Netflix_Logo_PMS from "./logo/Netflix_Logo_PMS.png"
 const Header=()=>{
-    return(<div><img src={Netflix_Logo_PMS} alt="Netflix Logo"/></div>)
+    return(<div><img className="absolute"src={Netflix_Logo_PMS} alt="Netflix Logo"/></div>)
 }
 
 export default Header;
--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "./Header";
+import Background_img from "./logo/Background_img.jpg"
 const Login=()=>{
     return(<div>
         <Header/>
-        Hello from the Login component
+        <div><img src={Background_img}/></div>
     </div>)
 }
 
