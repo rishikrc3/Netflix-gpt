f76f6703d45f2f8eabf3daa027fb6771b1c112d1
Rishik <rishikrc3@gmail.com>
2024-03-16 11:51:28 +0530

    bg-img

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Netflix_Logo_PMS from "./logo/Netflix_Logo_PMS.png"
 const Header=()=>{
-    return(<div><img className="absolute"src={Netflix_Logo_PMS} alt="Netflix Logo"/></div>)
+    return(<div className="absolute"><img src={Netflix_Logo_PMS} alt="Netflix Logo"/></div>)
 }
 
 export default Header;
